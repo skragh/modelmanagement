2c00e96ee10f667b48ecc626b75ee1699cdf9f97
skragh <83223908+skragh@users.noreply.github.com>
2021-12-06 13:00:00 +0100

    scaffolded router

--- src/main.js
+++ src/main.js
@@ -1,18 +1,12 @@
 import Vue from 'vue'
-import VueRouter from 'vue'
 import App from './App.vue'
-import Login from './components/Login.vue'
+import router from './router'
 
 Vue.config.productionTip = false
 
-const routes = [
-    {path: '/login',component:Login}]
 
-const router=new VueRouter({
-    routes
-})
 
 new Vue({
-    render: h => h(App),
     router,
+    render: h => h(App)
 }).$mount('#app');
